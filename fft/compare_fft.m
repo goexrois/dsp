6b13ad4fa6a7a340c91f82390ed80dba048ecdb2
Gonzalo <goexrois@manuela.home>
2017-04-06 20:19:57 -0300

    help pls

--- fft/compare_fft.m
+++ fft/compare_fft.m
@@ -1,12 +1,20 @@
 function compare_fft (bits) 
 	system(cstrcat("/home/goexrois/Documentos/dsp/fft/fft ",bits));
 	x = load("signal") ;
-	y = abs(fft(x)) ; 
+	numbits = str2num(bits) ; 	
+	y = abs(fft(x,numbits)) ;
 	my_y = load("out") ;
-	subplot(1,2,1) ;
+	x_axis = 0:1:numbits-1;
+	f_axis = (-numbits/2:1:(numbits-1)/2);
+	subplot(1,3,1) ;
+	plot(x_axis,x) ;
+	title("signal") ;
+	subplot(1,3,2) ;
 	plot(y) ;
+    y;
 	title("octave") ;
-	subplot(1,2,2) ;
+	subplot(1,3,3) ;
 	plot(my_y) ;
+	my_y;
 	title("fft para retards"); 
 endfunction
--- fft/fft.c
+++ fft/fft.c
@@ -17,12 +17,13 @@ typedef struct{
 
 void index_generator(int,int*) ;
 unsigned int reverse(unsigned int,int) ; 
-void butterfly(complex_t*, complex_t*, int, int) ;
+void butterfly(complex_t*, complex_t*, complex_t) ;
 void fft(complex_t*,int) ;
 float norma (complex_t c) ;
 void p_array(FILE*,complex_t* array,int size);
 void p_array_int(FILE*,int* array,int size);
 void p_array_float(FILE*,float* array,int size);
+void p_array_complex(FILE* fd,complex_t* array,int size);
 complex_t complex_add(complex_t, complex_t);
 complex_t complex_sub(complex_t, complex_t);
 complex_t complex_mult(complex_t x, complex_t y);
@@ -35,19 +36,23 @@ int main(int argc,char** argv){
 	float * signal_samples ;
 	complex_t * muestras ;
 	FILE* signal ;
+	float f = 100;
+	float T = 0.001;
 	debug = fopen("./debug","w+");
 	out = fopen("./out","w+");
 	signal = fopen("./signal","w+");
 
 	if( argc > 1 ){
-		length = atoi(argv[1]);  
+		length = atoi(argv[1]);
 	} 
 
 	signal_samples = (float*) malloc(sizeof(float)*length) ;
 	muestras = (complex_t *) malloc(sizeof(complex_t)*length) ;
 
 	for ( i = 0 ; i < length ; i++ ){
-		signal_samples[i] = sin(M_PI*2*(i+1)/length) ;
+	//	if( i+1 == length/2 ) signal_samples[i] = 1 ;
+	//	else	signal_samples[i] = sin(M_PI*8*(i+1-length/2)/length)/(M_PI*8*(i+1-length/2)/length) ;
+		signal_samples[i] = sin(M_PI*2*i*f/(float)length)+0.5*sin(M_PI*4*i*f/(float)length)+0.25*sin(M_PI*8*i*f/(float)length);
 		muestras[i].a = signal_samples[i] ; 
 		muestras[i].b = 0 ;
 	}
@@ -104,15 +109,9 @@ void print_complex (FILE* fd,complex_t z){
 	fprintf(fd,"%f+i%f\n",z.a,z.b);
 }
 
-void butterfly(complex_t* a0, complex_t* a1, int order, int n){
-	complex_t W ;
+void butterfly(complex_t* a0, complex_t* a1, complex_t W){
 	complex_t mult_aux ;
 	complex_t aux = *a0 ;
-
-	W.a = cos(2*M_PI*order/n) ;
-	W.b = -sin(2*M_PI*order/n) ;
-
-	print_complex(debug,W);
 
 	mult_aux = complex_mult(*a1, W) ;
 
@@ -124,19 +123,36 @@ void fft(complex_t* res,int length){
 	int i,j,k,power;
 	char niveles = log2(length) ; 
 	int * indexes = (int *) malloc(sizeof(int)*length) ;
+	complex_t W, aux, z,w; 
 	index_generator(length,indexes) ; 
 	p_array_int(debug,indexes,length);
 	for ( i = 1 ; i <= niveles ; i++ ){
+		fprintf(debug,"nivel%d------------------------\n",i);
 		power	= pow(2,i) ;
+		W.a = cos((float)2*M_PI/power) ;
+		fprintf(debug,"W.a=%f,",W.a);
+		W.b = -sin((float)2*M_PI/power) ;
+		fprintf(debug,"W.b=%f\n",W.b);
 		for( j = 0 ; j < length ; j+=power ){
+				aux.a = 1 ; 
+				aux.b = 0 ;
 			for( k = 0 ; k < power/2 ; k++ ) { 
-				fprintf(debug,"up=%d\tdown=%d\t,order=%d\t,n=%d\n",indexes[j+k],indexes[j+k+power/2],k,power) ;
- 				butterfly(&res[indexes[j+k]],&res[indexes[j+k+power/2]],k,power) ;
-				p_array(debug,res,length);
+				fprintf(debug,"up=%d\tdown=%d\t,order=%d\t,n=%d\n",indexes[j+k],indexes[j+k+power/2],k*length/power,length) ;
+ 				//butterfly(&res[indexes[j+k]],&res[indexes[j+k+power/2]],W) ;
+				
+				z = complex_mult(aux,res[indexes[j+k+power/2]]) ;
+				w = res[indexes[j+k]] ;
+				res[indexes[j+k]] = complex_add(z,w) ;
+				fprintf(debug,"res[%d]=w(%.2f+%.2fi) + W(%.2f+%.2fi)*res[%d](%.2f+%.2fi) = %.2f+%.2fi\n",
+					indexes[j+k],w.a,w.b,W.a,W.b,indexes[j+k+power/2],res[indexes[j+k+power/2]].a,res[indexes[j+k+power/2]].b,
+					res[indexes[j+k]].a,res[indexes[j+k]].b );
+				res[indexes[j+k+power/2]] = complex_sub(w,z) ;
+				fprintf(debug,"res[%d]=w(%.2f+%.2fi) - z(%.2f+%.2fi) = %.2f+%.2fi\n",
+					indexes[j+k+power/2],w.a,w.b,z.a,z.b,res[indexes[j+k+power/2]].a,res[indexes[j+k+power/2]].b ) ;
+				aux = complex_mult(aux,W) ;
+				p_array_complex(debug,res,length);
 			}
-			fprintf(debug,"JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ\n");
 		} 
-			fprintf(debug,"IIIIIIIIIIIIIIIIIIIIIIIIIIIIII\n");
 	}
 }
 
@@ -147,6 +163,15 @@ void p_array(FILE* fd,complex_t* array,int size){
 	}
 	fprintf(fd,"\n") ;
 }
+
+void p_array_complex(FILE* fd,complex_t* array,int size){
+	int i = 0 ;
+	for ( ; i < size ; i++ ){
+			fprintf(fd,"%.2f+%.2fi ",array[i].a,array[i].b) ;
+	}
+	fprintf(fd,"\n") ;
+}
+
 
 void p_array_int (FILE* fd, int* array,int size){
 	int i = 0 ;
